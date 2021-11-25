19ce2f880de73cfdbe2533226cc6303aa760506f
fon001 <48176372+fon001@users.noreply.github.com>
2021-11-25 18:44:04 +0000

    Update perf1.js

--- k6tests/perf1.js
+++ k6tests/perf1.js
@@ -52,7 +52,7 @@ export let options = {
 // Export a default function - this defines the entry point for your VUs,
 // similar to the main() function in many other languages.
 export default function() {
-  let res = http.get("https://bpcalculator20211123205548.azurewebsites.net");
+  let res = http.get("http://bpcalapp-dev.eu-west-1.elasticbeanstalk.com");
 
   check(res, {
       "is status 200": (r) => r.status === 200
